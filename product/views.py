cc1c94a77bf60a678d1b8dc3db638f658ffe7836
unknown <neilmagic25@gmail.com>
2023-03-18 01:08:38 -0400

    added somoe new content under the product table from the admim site of our django rest api, also testing with postman

--- product/views.py
+++ product/views.py
@@ -1,8 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import ProductSerializer
+from .models import Product
 
 @api_view(['GET'])
 def product_list(request):
+    supply = Product.objects.all()
 
+    serializer =  ProductSerializer(supply, many=True)
 
-    return Response('ok')
+    return Response(serializer.data)
+
